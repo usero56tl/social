094676c8a55502273cdf1de6bc00f46e27cf7169
Mao Zedong <c1378@pm.me>
2023-07-31 14:44:46 +0000

    change end of line

--- index.php
+++ index.php
@@ -3,12 +3,10 @@
         <title>Page Title</title>
     </head>
     <body>
-
         <h1>This is a Heading</h1>
         <p>This is a paragraph.</p>
         <?php
             echo 'hi';
         ?>
-
     </body>
 </html> 
